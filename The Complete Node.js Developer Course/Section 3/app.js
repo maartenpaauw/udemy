675c81a3f74a94c2ef0515b3107d0f37d4dd6d61
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-23 16:00:44 +0200

    chore(the-complete-nodejs-developer-course): Section 3 - Lecture 11

--- The Complete Node.js Developer Course/Section 3/app.js
+++ The Complete Node.js Developer Course/Section 3/app.js
@@ -2,6 +2,9 @@
 const fs = require('fs')
 const os = require('os')
 
+// NPM
+const _ = require('lodash')
+
 // Own
 const notes = require('./notes')
 
@@ -12,6 +15,11 @@ console.log(res)
 
 console.log('Result:', notes.sum(9, -2))
 
+console.log(_.isString(true))
+console.log(_.isString('Maarten Paauw'))
+
+const filteredArray = _.uniq([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])
+console.log(filteredArray)
 // const user = os.userInfo()
 
 // fs.appendFile('greetings.txt', `Hello ${user.username}! Your are ${notes.age}.`, (err) => {
