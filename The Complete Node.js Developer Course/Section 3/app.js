9b09249480557512824c1406b32430d6f7bf4681
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-23 16:07:37 +0200

    chore(the-complete-nodejs-developer-course): Section 3 - Lecture 12

--- The Complete Node.js Developer Course/Section 3/app.js
+++ The Complete Node.js Developer Course/Section 3/app.js
@@ -1,27 +1,5 @@
 // Build in
 const fs = require('fs')
-const os = require('os')
-
-// NPM
 const _ = require('lodash')
 
-// Own
 const notes = require('./notes')
-
-console.log('Starting application...')
-
-var res = notes.addNote()
-console.log(res)
-
-console.log('Result:', notes.sum(9, -2))
-
-console.log(_.isString(true))
-console.log(_.isString('Maarten Paauw'))
-
-const filteredArray = _.uniq([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])
-console.log(filteredArray)
-// const user = os.userInfo()
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! Your are ${notes.age}.`, (err) => {
-//   if (err) console.log(err.message)
-// })
--- The Complete Node.js Developer Course/Section 3/notes.js
+++ The Complete Node.js Developer Course/Section 3/notes.js
@@ -3,10 +3,5 @@ console.log('Starting notes...')
 // module.exports.age = 24
 
 module.exports.addNote = () => {
-  console.log('Add note function')
-  return 'New note.'
-}
-
-module.exports.sum = (a, b) => {
-  return a + b
+  
 }
