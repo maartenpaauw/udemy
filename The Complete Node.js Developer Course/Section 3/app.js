9245a3cb90be53e6c807b2c683ffb55af879b026
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-24 19:52:03 +0200

    chore(the-complete-nodejs-developer-course): Section 3 - Lecture 23

--- The Complete Node.js Developer Course/Section 3/app.js
+++ The Complete Node.js Developer Course/Section 3/app.js
@@ -5,8 +5,36 @@ const yargs = require('yargs')
 
 const notes = require('./notes')
 
-const argv = yargs.argv
+const title = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+}
+
+const body = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+}
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title,
+    body
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title
+  })
+  .command('remove', 'Remove a note', {
+    title
+  })
+  .help()
+  .argv
+
 const command = argv._[0]
+
+yargs.command()
 
 if (command === 'add') {
   const note = notes.addNote(argv.title, argv.body)
