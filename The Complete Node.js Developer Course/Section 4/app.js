ed2025cf77dd496deb950c8bd3e8217b67102f4b
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-25 21:48:21 +0200

    chore(the-complete-nodejs-developer-course): Section 4 - Lecture 29

--- The Complete Node.js Developer Course/Section 4/app.js
+++ The Complete Node.js Developer Course/Section 4/app.js
@@ -4,5 +4,5 @@ request({
   url: 'http://maps.googleapis.com/maps/api/geocode/json?address=37%20brouwerstraat%20nederland',
   json: true
 }, (error, response, body) => {
-  console.log(body)
+  console.log(JSON.stringify(body, undefined, 2))
 })
