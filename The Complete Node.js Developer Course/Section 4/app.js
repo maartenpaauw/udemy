7918f83d576be5f9e2ed9345a0c9f51f1a0282e9
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-26 20:35:26 +0200

    chore(the-complete-nodejs-developer-course): Section 4 - Lecture 32

--- The Complete Node.js Developer Course/Section 4/app.js
+++ The Complete Node.js Developer Course/Section 4/app.js
@@ -18,7 +18,17 @@ request({
   url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`)
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+  if (error) {
+    console.log('Unable to connect to Google Services...')
+  }
+
+  else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address...')
+  }
+
+  else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+  }
 })
