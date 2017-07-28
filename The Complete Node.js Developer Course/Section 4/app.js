ccc3de67d231c12a18b107857fedd4235ab9101c
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-28 15:00:43 +0200

    chore(the-complete-nodejs-developer-course): Section 4 - Lecture 34

--- The Complete Node.js Developer Course/Section 4/app.js
+++ The Complete Node.js Developer Course/Section 4/app.js
@@ -1,26 +1,46 @@
-// NPM
-const yargs = require('yargs')
+// // NPM
+// const yargs = require('yargs')
 
-// Custom
-const geocode = require('./geocode/geocode')
+// // Custom
+// const geocode = require('./geocode/geocode')
 
-const argv = yargs
-  .options({
-    address: {
-      demand: true,
-      alias: 'a',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv
+// .env
+require('dotenv').config()
 
-geocode.geocodeAddress(argv.address, (error, results) => {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log(JSON.stringify(results, undefined, 2))
+// console.log(process.env.DARK_SKY_API_KEY)
+
+// const argv = yargs
+//   .options({
+//     address: {
+//       demand: true,
+//       alias: 'a',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv
+
+// geocode.geocodeAddress(argv.address, (error, results) => {
+//   if (error) {
+//     console.log(error)
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2))
+//   }
+// })
+
+const request = require('request')
+
+request({
+  url: `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/37.8267,-122.4233`,
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature)
+  }
+
+  else {
+    console.log('Unable to fetch weather...')
   }
 })
