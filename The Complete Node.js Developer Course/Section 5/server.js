6d27bc91ab7498569662bfd384db5699182e274c
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-28 20:07:56 +0200

    chore(the-complete-nodejs-developer-course): Section 5 - Lecture 45

--- The Complete Node.js Developer Course/Section 5/server.js
+++ The Complete Node.js Developer Course/Section 5/server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 const app = express()
 
 hbs.registerPartials(`${__dirname}/views/partials`)
 app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const message = `${now}: ${req.method} ${req.url}`
+  console.log(message)
+
+  fs.appendFileSync('server.log', `${message}\n`)
+  next()
+})
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// })
+
 app.use(express.static(`${__dirname}/public`))
 
 hbs.registerHelper('year', () => {
