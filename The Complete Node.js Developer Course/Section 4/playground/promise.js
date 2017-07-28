7c6831f5cf8f571fe48ed2b322d3c82ac12b62a4
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-28 15:30:34 +0200

    chore(the-complete-nodejs-developer-course): Section 4 - Lecture 36

---
+++ The Complete Node.js Developer Course/Section 4/playground/promise.js
@@ -0,0 +1,12 @@
+var somePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hey. It worked!')
+    // reject('Unable to fulfill promise...')
+  }, 2500)
+})
+
+somePromise.then((message) => {
+  console.log('Success:', message)
+}).catch((message) => {
+  console.log('Failed:', message)
+})
