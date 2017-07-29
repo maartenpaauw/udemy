bd598c3c031de473ca90a01e3fd1966b95c21506
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-29 15:37:48 +0200

    chore(the-complete-nodejs-developer-course): Section 6 - Lecture 54

--- The Complete Node.js Developer Course/Section 6/utils/utils.js
+++ The Complete Node.js Developer Course/Section 6/utils/utils.js
@@ -1,6 +1,20 @@
 module.exports.add = (a, b) => a + b
+
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b)
+  }, 1000)
+}
+
 module.exports.subtract = (a, b) => a - b
 module.exports.square = (a, b) => a * b
+
+module.exports.asyncSquare = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a * b)
+  }, 1000)
+}
+
 
 module.exports.setName = (user, fullName) => {
   var names = fullName.split(" ")
--- The Complete Node.js Developer Course/Section 6/utils/utils.test.js
+++ The Complete Node.js Developer Course/Section 6/utils/utils.test.js
@@ -6,6 +6,13 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number')
 })
 
+it('should add two numbers asynchronous', (done) => {
+  utils.asyncAdd(4, 3, (res) => {
+    expect(res).toBe(7).toBeA('number')
+    done()
+  })
+})
+
 it('should subtract two numbers', () => {
   const res = utils.subtract(33, 11)
   expect(res).toBe(22).toBeA('number')
@@ -14,6 +21,13 @@ it('should subtract two numbers', () => {
 it('should square two numbers', () => {
   const res = utils.square(2, 2)
   expect(res).toBe(4).toBeA('number')
+})
+
+it('should square two numbers asynchronous', (done) => {
+  utils.asyncSquare(2, 2, (res) => {
+    expect(res).toBe(4).toBeA('number')
+    done()
+  })
 })
 
 it('should expect some values', () => {
