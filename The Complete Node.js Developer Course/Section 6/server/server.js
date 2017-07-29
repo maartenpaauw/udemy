0babbfc6a6118ec4e3cae766910976970aa61b36
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-29 17:52:11 +0200

    chore(the-complete-nodejs-developer-course): Section 6 - Lecture 56

--- The Complete Node.js Developer Course/Section 6/server/server.js
+++ The Complete Node.js Developer Course/Section 6/server/server.js
@@ -3,9 +3,27 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'TODO App v1.0'
+  })
+})
+
+app.get('/users', (req, res) => {
+  res.send([
+    {
+      name: 'Maarten Paauw',
+      age: 24
+    },
+    {
+      name: 'Rianne van der Maat',
+      age: 23
+    }
+  ])
 })
 
 app.listen(3000, () => {
   console.log('Server up and running on http://localhost:3000/')
 })
+
+module.exports.app = app
