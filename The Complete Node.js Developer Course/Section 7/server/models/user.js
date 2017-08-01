e315602bf0b5558924b76a3e6f63c8c9a37ca302
Maarten Paauw <maartenpaauw@gmail.com>
2017-08-01 22:03:55 +0200

    chore(the-complete-nodejs-developer-course): Section 7 - Lecture 90

--- The Complete Node.js Developer Course/Section 7/server/models/user.js
+++ The Complete Node.js Developer Course/Section 7/server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
-const User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -30,6 +32,32 @@ const User = mongoose.model('User', {
     }
   }]
 })
+
+UserSchema.methods.toJSON = function () {
+  const user = this
+  const userObject = user.toObject()
+  return _.pick(userObject, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  const user = this
+  const access = 'auth'
+  const token = jwt.sign({
+    _id: user._id.toHexString(),
+    access
+  }, 'abc123').toString()
+
+  user.tokens.push({
+    access,
+    token
+  })
+
+  return user.save().then(() => {
+    return token
+  });
+}
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = {
   User
--- The Complete Node.js Developer Course/Section 7/server/server.js
+++ The Complete Node.js Developer Course/Section 7/server/server.js
@@ -119,12 +119,14 @@ app.post('/users', (req, res) => {
   const body = _.pick(req.body, ['email', 'password'])
   const user = new User(body)
 
-  user.save().then((user) => {
-    res.send(user)
-  }).catch((err) => {
-    res.status(400).send(err)
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
   })
-})
+});
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}/`)
