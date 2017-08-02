90cebe6e808c75fc25a0386661f32972bf7c13a9
Maarten Paauw <maartenpaauw@gmail.com>
2017-08-02 19:22:30 +0200

    chore(the-complete-nodejs-developer-course): Section 7 - Lecture 96

--- The Complete Node.js Developer Course/Section 7/server/tests/server.test.js
+++ The Complete Node.js Developer Course/Section 7/server/tests/server.test.js
@@ -234,7 +234,7 @@ describe('Server', () => {
             expect(user).toExist()
             expect(user.password).toNotBe(password)
             done()
-          })
+          }).catch((e) => done(e))
         })
     })
 
@@ -259,5 +259,58 @@ describe('Server', () => {
         .expect(400)
         .end(done)
     })
+  })
+
+  describe('POST /users/login', () => {
+
+    it('should login user and return auth token', (done) => {
+      request(app)
+        .post('/users/login')
+        .send({
+          email: users[1].email,
+          password: users[1].password
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.headers['x-auth']).toExist()
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err)
+          }
+
+          User.findById(users[1]._id).then((user) => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            })
+            done()
+          }).catch((e) => done(e))
+        })
+    })
+
+    it('should reject invalid login', (done) => {
+      request(app)
+        .post('/users/login')
+        .send({
+          email: users[1].email,
+          password: users[1].password + '345'
+        })
+        .expect(400)
+        .expect((res) => {
+          expect(res.headers['x-auth']).toNotExist()
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err)
+          }
+
+          User.findById(users[1]._id).then((user) => {
+            expect(user.tokens.length).toBe(0)
+            done()
+          }).catch((e) => done(e))
+        })
+    })
+
   })
 })
