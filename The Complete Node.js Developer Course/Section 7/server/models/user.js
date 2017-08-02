e9412bb3b5ded39ff7aa1fa93719baa9023e48e7
Maarten Paauw <maartenpaauw@gmail.com>
2017-08-02 19:31:26 +0200

    chore(the-complete-nodejs-developer-course): Section 7 - Lecture 97

--- The Complete Node.js Developer Course/Section 7/server/models/user.js
+++ The Complete Node.js Developer Course/Section 7/server/models/user.js
@@ -58,6 +58,15 @@ UserSchema.methods.generateAuthToken = function () {
   });
 }
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  })
+}
+
 UserSchema.statics.findByToken = function (token) {
   var User = this
   var decoded
