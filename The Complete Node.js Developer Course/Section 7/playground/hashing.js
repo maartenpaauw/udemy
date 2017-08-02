b2394d16fa5f17a634f526b6053e862f7deb8857
Maarten Paauw <maartenpaauw@gmail.com>
2017-08-02 15:55:33 +0200

    chore(the-complete-nodejs-developer-course): Section 7 - Lecture 92

--- The Complete Node.js Developer Course/Section 7/playground/hashing.js
+++ The Complete Node.js Developer Course/Section 7/playground/hashing.js
@@ -1,15 +1,30 @@
 const { SHA256 } = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id: 10
-}
+const password = 'secret-123'
 
-const token = jwt.sign(data, 'secret')
-console.log(token)
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash)
+  })
+})
 
-const decoded = jwt.verify(token, 'secret')
-console.log(decoded)
+const hashedPassword = '$2a$10$guyMJ4hLsS6FXFjeUi.aG.OPTiKT6bot69PtGnHiX18RjXMT2IeZO'
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+  console.log(result)
+})
+
+// var data = {
+//   id: 10
+// }
+
+// const token = jwt.sign(data, 'secret')
+// console.log(token)
+
+// const decoded = jwt.verify(token, 'secret')
+// console.log(decoded)
 
 // const message = 'I am user number 3'
 // const hash = SHA256(message).toString()
