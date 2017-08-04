7d488b539f06d98f8e440c359b33c1ac51c59534
Maarten Paauw <maartenpaauw@gmail.com>
2017-08-04 22:10:26 +0200

    chore(the-complete-nodejs-developer-course): Section 7 - Lecture 112

--- The Complete Node.js Developer Course/Section 9/public/js/index.js
+++ The Complete Node.js Developer Course/Section 9/public/js/index.js
@@ -1,4 +1,4 @@
-var socket = io()
+const socket = io()
 
 socket.on('connect', function () {
   console.log('Connected to server')
@@ -10,11 +10,25 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('Message', message)
+  var li = $('<li></li>')
+  li.text(`${message.from}: ${message.text}`)
+  $('#messages').append(li)
 })
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi!'
-}, function (message) {
-  console.log('Got it!', message)
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'Hi!'
+// }, function (message) {
+//   console.log('Got it!', message)
+// })
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault()
+
+  socket.emit('createMessage',  {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+    $('[name=message]').val('')
+  })
 })
