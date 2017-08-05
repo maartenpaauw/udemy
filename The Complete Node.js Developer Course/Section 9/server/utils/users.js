0a60cfb63a374af743e370908a683389800eb8e1
Maarten Paauw <maartenpaauw@gmail.com>
2017-08-05 17:06:18 +0200

    chore(the-complete-nodejs-developer-course): Section 9 - Lecture 124

--- The Complete Node.js Developer Course/Section 9/server/utils/users.js
+++ The Complete Node.js Developer Course/Section 9/server/utils/users.js
@@ -4,12 +4,30 @@ class Users {
     this.users = []
   }
 
-  addUser(id, name, room) {
+  addUser (id, name, room) {
     const user = { id, name, room }
     this.users.push(user)
     return user
   }
 
+  removeUser (id) {
+    const user = this.getUser(id)
+    
+    if(user) {
+      this.users = this.users.filter((user) => user.id !== id)
+    }
+
+    return user
+  }
+
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+
+  getUsersList (room) {
+    const users = this.users.filter((user) => user.room === room)
+    return users.map((user) => user.name)
+  }
 }
 
 module.exports = {
--- The Complete Node.js Developer Course/Section 9/server/utils/users.test.js
+++ The Complete Node.js Developer Course/Section 9/server/utils/users.test.js
@@ -3,6 +3,27 @@ const { Users } = require('./users')
 
 describe('Users', () => {
 
+  var users
+
+  beforeEach(() => {
+    users = new Users()
+    users.users = [{
+      id: 1,
+      name: 'User 1',
+      room: 'A'
+    },
+    {
+      id: 2,
+      name: 'User 2',
+      room: 'B'
+    },
+    {
+      id: 3,
+      name: 'User 3',
+      room: 'B'
+    }]
+  })
+
   describe('Users.addUser()', () => {
 
     it('should add new user', () => {
@@ -14,6 +35,50 @@ describe('Users', () => {
       }
       users.addUser(user.id, user.name, user.room)
       expect(users.users).toEqual([user])
+    })
+
+  })
+
+  describe('Users.removeUser()', () => {
+
+    it('should remove a user', () => {
+      const user = users.removeUser(1)
+      expect(user.id).toBe(1)
+      expect(users.users.length).toBe(2)
+    })
+
+    it('should not remove a user', () => {
+      const user = users.removeUser(4)
+      expect(user).toNotExist()
+      expect(users.users.length).toBe(3)
+    })
+
+  })
+
+  describe('Users.getUser()', () => {
+    
+    it('should find a user', () => {
+      const user = users.getUser(2)
+      expect(user.id).toBe(2)
+    })
+
+    it('should not find a user', () => {
+      const user = users.getUser(4)
+      expect(user).toNotExist()
+    })
+
+  })
+
+  describe('Users.getUsersList()', () => {
+    
+    it('should return names for A', () => {
+      const list = users.getUsersList('A')
+      expect(list).toEqual(['User 1'])
+    })
+
+    it('should return names for B', () => {
+      const list = users.getUsersList('B')
+      expect(list).toEqual(['User 2', 'User 3'])
     })
 
   })
