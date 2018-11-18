bb76ee631435faeab2c3dce967264ca9411109ea
maartenpaauw <maartenpaauw@gmail.com>
2018-11-18 20:08:00 +0100

    chore(the-complete-react-web-developer-course): section 3 - lecture 9

--- the-complete-react-web-developer-course/indecision-app/public/scripts/app.js
+++ the-complete-react-web-developer-course/indecision-app/public/scripts/app.js
@@ -1,4 +1,10 @@
-var template = <p>test</p>;
+'use strict';
+
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
