333f69781aaa3e4f4f40f2d1b15c600b98b3ac00
maartenpaauw <maartenpaauw@gmail.com>
2018-11-18 20:18:25 +0100

    chore(the-complete-react-web-developer-course): section 3 - lecture 10

--- the-complete-react-web-developer-course/indecision-app/src/app.js
+++ the-complete-react-web-developer-course/indecision-app/src/app.js
@@ -1,4 +1,22 @@
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Maarten Paauw</h1>
+        <p>Age: 25</p>
+        <p>Location: The Netherlands</p>
+    </div>
+)
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
