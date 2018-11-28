fb054bdce3f8613cc01d5281829be98ba81aa765
Maarten Paauw <maartenpaauw@gmail.com>
2018-11-28 11:19:10 +0100

    chore(the-complete-react-web-developer-course): section 3 - lecture 12

--- the-complete-react-web-developer-course/indecision-app/public/scripts/app.js
+++ the-complete-react-web-developer-course/indecision-app/public/scripts/app.js
@@ -2,14 +2,22 @@
 
 var app = {
   title: 'Indecision App',
-  subtitle: 'This is some info'
+  subtitle: 'This is some info',
+  options: ['One', 'Two']
 };
-var template = React.createElement("div", null, React.createElement("h1", null, app.title), React.createElement("p", null, app.subtitle), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
+var template = React.createElement("div", null, React.createElement("h1", null, app.title), app.subtitle && React.createElement("p", null, app.subtitle), React.createElement("p", null, app.options.length > 0 ? 'Here are your options:' : 'No options.'), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
 var user = {
   name: 'Maarten Paauw',
   age: 25,
   location: 'The Netherlands'
 };
-var templateTwo = React.createElement("div", null, React.createElement("h1", null, user.name), React.createElement("p", null, "Age: ", user.age), React.createElement("p", null, "Location: ", user.location));
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement("p", null, "Location: ", location);
+  }
+}
+
+var templateTwo = React.createElement("div", null, React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- the-complete-react-web-developer-course/indecision-app/src/app.js
+++ the-complete-react-web-developer-course/indecision-app/src/app.js
@@ -1,12 +1,14 @@
 var app = {
     title: 'Indecision App',
     subtitle: 'This is some info',
+    options: ['One', 'Two']
 }
 
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options:' : 'No options.'}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -20,11 +22,17 @@ var user = {
     location: 'The Netherlands',
 };
 
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
 var templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
