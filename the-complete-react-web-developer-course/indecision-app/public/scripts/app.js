b5b7438ad40c92f452c9692fa28ea4ce83a0d13a
Maarten Paauw <maartenpaauw@gmail.com>
2018-11-28 10:55:21 +0100

    fix: use the latest babel version

--- the-complete-react-web-developer-course/indecision-app/public/scripts/app.js
+++ the-complete-react-web-developer-course/indecision-app/public/scripts/app.js
@@ -1,67 +1,15 @@
-'use strict';
+"use strict";
 
 var app = {
-    title: 'Indecision App',
-    subtitle: 'This is some info'
+  title: 'Indecision App',
+  subtitle: 'This is some info'
 };
-
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
-);
-
+var template = React.createElement("div", null, React.createElement("h1", null, app.title), React.createElement("p", null, app.subtitle), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
 var user = {
-    name: 'Maarten Paauw',
-    age: 25,
-    location: 'The Netherlands'
+  name: 'Maarten Paauw',
+  age: 25,
+  location: 'The Netherlands'
 };
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
-);
-
+var templateTwo = React.createElement("div", null, React.createElement("h1", null, user.name), React.createElement("p", null, "Age: ", user.age), React.createElement("p", null, "Location: ", user.location));
 var appRoot = document.getElementById('app');
-
 ReactDOM.render(template, appRoot);
