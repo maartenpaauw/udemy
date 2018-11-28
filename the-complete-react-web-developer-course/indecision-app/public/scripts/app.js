c3a81a7082471216df199afa4ed17422521ff848
Maarten Paauw <maartenpaauw@gmail.com>
2018-11-28 16:29:06 +0100

    chore(the-complete-react-web-developer-course): section 3 - lecture 16

--- the-complete-react-web-developer-course/indecision-app/public/scripts/app.js
+++ the-complete-react-web-developer-course/indecision-app/public/scripts/app.js
@@ -6,18 +6,26 @@ var app = {
   options: ['One', 'Two']
 };
 var template = React.createElement("div", null, React.createElement("h1", null, app.title), app.subtitle && React.createElement("p", null, app.subtitle), React.createElement("p", null, app.options.length > 0 ? 'Here are your options:' : 'No options.'), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
-var user = {
-  name: 'Maarten Paauw',
-  age: 25,
-  location: 'The Netherlands'
+var count = 0;
+
+var addOne = function addOne() {
+  console.log('Add one');
 };
 
-function getLocation(location) {
-  if (location) {
-    return React.createElement("p", null, "Location: ", location);
-  }
-}
+var minusOne = function minusOne() {
+  console.log('Minus one');
+};
 
-var templateTwo = React.createElement("div", null, React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
+var reset = function reset() {
+  console.log('Reset');
+};
+
+var templateTwo = React.createElement("div", null, React.createElement("h1", null, "Count: ", count), React.createElement("button", {
+  onClick: addOne
+}, "+1"), React.createElement("button", {
+  onClick: minusOne
+}, "-1"), React.createElement("button", {
+  onClick: reset
+}, "Reset"));
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
