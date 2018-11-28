e9c3d51f4135a70be23f10f7a331a30684ddcd89
Maarten Paauw <maartenpaauw@gmail.com>
2018-11-28 13:38:52 +0100

    chore(the-complete-react-web-developer-course): section 3 - lecture 13

--- the-complete-react-web-developer-course/indecision-app/src/app.js
+++ the-complete-react-web-developer-course/indecision-app/src/app.js
@@ -1,10 +1,10 @@
-var app = {
+const app = {
     title: 'Indecision App',
     subtitle: 'This is some info',
     options: ['One', 'Two']
 }
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
@@ -16,7 +16,7 @@ var template = (
     </div>
 );
 
-var user = {
+const user = {
     name: 'Maarten Paauw',
     age: 25,
     location: 'The Netherlands',
@@ -28,7 +28,7 @@ function getLocation(location) {
     }
 }
 
-var templateTwo = (
+const templateTwo = (
     <div>
         <h1>{user.name ? user.name : 'Anonymous'}</h1>
         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
@@ -36,6 +36,6 @@ var templateTwo = (
     </div>
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
