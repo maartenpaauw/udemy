060f9206a9e4b11103b3d617b06a54377b346ae1
Maarten Paauw <maartenpaauw@gmail.com>
2018-12-07 16:03:05 +0100

    chore(the-complete-react-web-developer-course): section 9 - lecture 77

--- the-complete-react-web-developer-course/expensify-app/src/app.js
+++ the-complete-react-web-developer-course/expensify-app/src/app.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    ExpenseDashboardPage
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    AddExpensePage
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    EditExpensePage
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    HelpPage
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
--- the-complete-react-web-developer-course/expensify-app/webpack.config.js
+++ the-complete-react-web-developer-course/expensify-app/webpack.config.js
@@ -26,5 +26,6 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
   },
 };
