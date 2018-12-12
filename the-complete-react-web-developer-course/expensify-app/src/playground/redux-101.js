ae8f55a8f27610f931442ed7821acf95860251f0
Maarten Paauw <maartenpaauw@gmail.com>
2018-12-12 11:36:05 +0100

    chore(the-complete-react-web-developer-course): section 10 - lecture 90

--- the-complete-react-web-developer-course/expensify-app/src/playground/redux-101.js
+++ the-complete-react-web-developer-course/expensify-app/src/playground/redux-101.js
@@ -1,16 +1,33 @@
 import { createStore } from 'redux';
 
+const increment = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy,
+});
+
+const decrement = ({ decrementBy = 1} = {}) => ({
+  type: 'DECREMENT',
+  decrementBy,
+});
+
+const reset = () => ({
+  type: 'RESET',
+});
+
+const set = ({ count }) => ({
+  type: 'SET',
+  count,
+});
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy,
+        count: state.count + action.incrementBy,
       }
       case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy,
+        count: state.count - action.decrementBy,
       }
     case 'RESET':
       return {
@@ -29,33 +46,16 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5,
-});
+store.dispatch(increment({ incrementBy: 5 }));
 
-store.dispatch({
-  type: 'INCREMENT',
-});
+store.dispatch(increment());
 
-store.dispatch({
-  type: 'DECREMENT',
-});
+store.dispatch(decrement());
 
-store.dispatch({
-  type: 'INCREMENT',
-});
+store.dispatch(increment());
 
-store.dispatch({
-  type: 'RESET',
-});
+store.dispatch(reset());
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10,
-});
+store.dispatch(decrement({ decrementBy: 10 }));
 
-store.dispatch({
-  type: 'SET',
-  count: 10,
-})
+store.dispatch(set({ count: 101 }));
