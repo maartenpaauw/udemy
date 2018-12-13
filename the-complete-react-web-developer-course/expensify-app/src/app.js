935bde761f643c28b34d8d8fb81cff998c1c5793
Maarten Paauw <maartenpaauw@gmail.com>
2018-12-13 15:32:15 +0100

    chore(the-complete-react-web-developer-course): section 11 - lecture 99

--- the-complete-react-web-developer-course/expensify-app/src/app.js
+++ the-complete-react-web-developer-course/expensify-app/src/app.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
 
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import { getVisibleExpenses } from './selectors/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water bill'}));
+store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(setTextFilter('water'));
+
+const { expenses, filters } = store.getState();
+const visibleExpenses = getVisibleExpenses(expenses, filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
