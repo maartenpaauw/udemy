fe716d594138e9f8d0dded3100770502ba9f0c16
maartenpaauw <maartenpaauw@gmail.com>
2018-12-18 23:10:34 +0100

    chore(the-complete-react-web-developer-course): section 12 - lecture 113

--- the-complete-react-web-developer-course/expensify-app/src/actions/expenses.js
+++ the-complete-react-web-developer-course/expensify-app/src/actions/expenses.js
@@ -1,11 +1,11 @@
 import uuid from 'uuid';
 
-export const addExpense = ({ description = '', notes = '', amount = 0, createdAt = 0 } = {}) => ({
+export const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0 } = {}) => ({
   type: 'ADD_EXPENSE',
   expense: {
     id: uuid(),
     description,
-    notes,
+    note,
     amount,
     createdAt,
   }
