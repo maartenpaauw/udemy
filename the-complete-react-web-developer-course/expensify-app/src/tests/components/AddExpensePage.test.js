478587a17650c1f0ed9d7a54f29334ad32b8983c
maartenpaauw <maartenpaauw@gmail.com>
2019-01-03 22:33:00 +0100

    chore(the-complete-react-web-developer-course): section 12 - lecture 125

--- the-complete-react-web-developer-course/expensify-app/src/tests/components/AddExpensePage.test.js
+++ the-complete-react-web-developer-course/expensify-app/src/tests/components/AddExpensePage.test.js
@@ -4,14 +4,14 @@ import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
-let onSubmitSpy, historySpy, wrapper;
+let addExpenseSpy, historySpy, wrapper;
 
 beforeEach(() => {
-    onSubmitSpy = jest.fn();
+    addExpenseSpy = jest.fn();
     historySpy = {
         push: jest.fn(),
     };
-    wrapper = shallow(<AddExpensePage onSubmit={onSubmitSpy} history={historySpy} />);
+    wrapper = shallow(<AddExpensePage addExpense={addExpenseSpy} history={historySpy} />);
 });
 
 test('should render AddExpensePage correctly', () => {
@@ -21,5 +21,5 @@ test('should render AddExpensePage correctly', () => {
 test('should handle onSubmit', () => {
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
     expect(historySpy.push).toHaveBeenLastCalledWith('/');
-    expect(onSubmitSpy).toHaveBeenLastCalledWith(expenses[1]);
+    expect(addExpenseSpy).toHaveBeenLastCalledWith(expenses[1]);
 });
